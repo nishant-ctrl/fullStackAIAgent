84890a9fb59907c26a062e90321240a1916d4159
nishant-ctrl <knishant9135@gmail.com>
2025-07-08 23:05:08 +0530

    add ticket and user models

--- ai-ticket-assistant/models/ticket.model.js
+++ ai-ticket-assistant/models/ticket.model.js
@@ -1,9 +1,38 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const ticketSchema = new mongoose.Schema(
     {
-        
+        title: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        status: {
+            type: String,
+            default: "TODO",
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        assignedTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: null,
+        },
+        priority: {
+            type: String,
+        },
+        deadline: {
+            type: Date,
+        },
+        helpfulNotes: {
+            type: String,
+        },
+        relatedSkills: [String],
     },
-    {timestamps:true});
+    { timestamps: true }
+);
 
-export const Ticket=mongoose.model("Ticket",ticketSchema)
+export const Ticket = mongoose.model("Ticket", ticketSchema);
--- ai-ticket-assistant/models/user.model.js
+++ ai-ticket-assistant/models/user.model.js
@@ -1,9 +1,24 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
-        
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: "user",
+            enum: ["user", "admin", "moderator"],
+        },
+        skills: [String],
     },
-    {timestamps:true});
+    { timestamps: true }
+);
 
-export const User=mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema);
